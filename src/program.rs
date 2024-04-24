2e5ad6ead1f1a284d8760803a114449156fb2526
PooriaGG <104061307+pooriagg@users.noreply.github.com>
2024-04-24 10:57:02 +0330

    Update program.rs

--- src/program.rs
+++ src/program.rs
@@ -3,7 +3,7 @@ use solana_program::{
     pubkey::Pubkey
 };
 
-pub const PROGRAM_ID: Pubkey = pubkey!("DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263");
+pub const PROGRAM_ID: Pubkey = pubkey!("<Pubkey>");
 
 pub fn check_id(program_id: &Pubkey) -> bool {
     *program_id == PROGRAM_ID
